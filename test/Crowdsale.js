73d66a8f6b23c6c9cd1750785a740f2fd0add7d7
Dave Miner <wtrispsn@gmail.com>
2025-06-07 12:54:52 -0400

    Adds Crowdsale contract

---
+++ test/Crowdsale.js
@@ -0,0 +1,20 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+describe("Crowdsale", function () {
+  let crowdsale;
+  beforeEach(async function () {
+    const Crowdsale = await ethers.getContractFactory("Crowdsale");
+    crowdsale = await Crowdsale.deploy();
+  });
+
+  describe("Deployment", function () {
+    it("has correct name", async function () {
+      expect(await crowdsale.name()).to.equal("Crowdsale");
+    });
+  });
+});
